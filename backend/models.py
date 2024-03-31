c4da4e2af81ca78b7f1635978c212161da49cb87
Aleks-SM <124567282+Aleks-SM@users.noreply.github.com>
2024-03-31 15:45:24 +0400

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 # Create your models here.
 
+class User:
+    pass
+
 class Shop(models.Model):
     name = models.CharField(max_length=50, verbose_name='Название')
     url = models.CharField(max_length=200, verbose_name='Ссылка', null=True, blank=True)
